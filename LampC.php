26265d5095741120cc288587bbc1198e78f2545e
Mekhi Marolyne <mekiceng99@gmail.com>
2022-05-17 15:40:03 +0800

    new commit

---
+++ LampC.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>LAMPIRAN A GENERATOR</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">  
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" />
+
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js" crossorigin="anonymous"></script>
+
+  <style>
+      @import url('https://rsms.me/inter/inter.css');
+      html { font-family: 'Inter', sans-serif; }
+
+      @supports (font-variation-settings: normal) {
+        html { font-family: 'Inter var', sans-serif; }
+        body { padding: 10px; }
+      }
+
+    .text-line {
+        background-color: transparent;
+        width: 51%;
+        color: #000000;
+        outline: none;
+        outline-style: none;
+        border-top: none;
+        border-left: none;
+        border-right: none;
+        border-bottom: solid #000000 1px;
+        text-decoration: none;
+        padding: 3px 5px;
+    }
+      .text-line1 {
+        background-color: transparent;
+        width: 31%;
+        color: #000000;
+        outline: none;
+        outline-style: none;
+        border-top: none;
+        border-left: none;
+        border-right: none;
+        border-bottom: solid #000000 1px;
+        text-decoration: none;
+        padding: 3px 5px;
+    }
+  </style>
+  </head>
+
+<body>
+<!-- Page Header -->
+<header>
+  <div class="p-5 text-center" >
+    <h2 class="mb-1" style="font-size:16px">Borang Pengesahan Akaun Bank Untuk Pembayaran</h2>
+    <h1 class="mb-1" style="font-size:22px">
+      <b>Untuk Pembayaran Am</b>
+    </h1>
+    <h3 class="mb-3" style="font-size:18px">
+      <u>Melalui Alliance Online Banking</u>
+    </h3>
+  </div>
+</header>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
+
